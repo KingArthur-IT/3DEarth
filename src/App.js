7fffc56336d94ba9736c15d1dd5522d6a7fdbc7e
Artem <OstapT5@gmail.com>
2023-08-29 23:13:07 +0200

    edits

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 
-var canvas, renderer, scene, camera, helicopterObj,
-sceneSize = {
-    width: 0,
-    height: 0
-},
-mouseData = {
-    isClicked: false,
-    x: 0,
-    y: 0,
-    isTouchScale: false,
-    touchScaleVal: 0
-},
-settings = {
-    zoomStep: 50,
-    zoomMax: 3000,
-    zoomMin: 1300,
-    moveStep: {
-        x: 0.05,
-        y: 0.01
+var canvas, renderer, scene, camera, EarthObj
+var deltaX = 0,
+    currentDeltaX = 0,
+    sceneSize = {
+        width: 0,
+        height: 0
     },
-    maxYRotate: -83.0 * Math.PI / 180.0, //down
-    minYRotate: -93.0 * Math.PI / 180.0,
-    maxXRotate: 70.0 * Math.PI / 180.0,
-    minXRotate: -70.0 * Math.PI / 180.0,
-    aspectRatio: 1,
-    camera: {
-        deep: 10000,
-        posY: 0,
-        posZ: 600
+    mouseData = {
+        x: 0,
+    },
+    settings = {
+        moveStep: {
+            x: 0.3,
+        },
+        aspectRatio: 1,
+        camera: {
+            deep: 10000,
+            posY: 0,
+            posZ: 600
+        }
     }
-}
 
 class App {
     init() {
@@ -54,132 +44,56 @@ class App {
         light.position.set(0, 0, 0);
         scene.add(light);
 
-        // const light2 = new THREE.PointLight(0xffffff, 1.0);
-        // light2.position.set(0, 10000, 0);
-        // light2.castShadow = true;
-        // scene.add(light2);
-
         renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
-        renderer.setClearColor( 0x000000, 0 );
+        // renderer.setClearColor( 0x000000, 0 );
         renderer.setPixelRatio( Math.min(window.devicePixelRatio, 1.5) );
         renderer.setSize( sceneSize.width, sceneSize.height );
 
         //obj
-        let EarthObj = new THREE.Object3D();
+        EarthObj = new THREE.Object3D();
         const objLoader = new OBJLoader();
+
+        const redMaterial = new THREE.MeshPhongMaterial({
+            color: 15017491,
+            shininess: .75,
+            transparent: false,
+            emissive: 15017491,
+            emissiveIntensity: 1
+        });
+
+        const whiteMaterial = new THREE.MeshPhongMaterial({
+            color: '#cccccc',
+        });
 
         // objLoader.setMaterials(materials);
         objLoader.load('./assets/Earth.obj', function (object) {
-            const scale = 2
+            const scale = 2.2
             object.scale.set(scale, scale, scale);
             object.position.set(0, 0, 0);
-            object.rotation.set(0, 0.4, 3.5);
+            object.rotation.set(0, 0.6, 3.5);
+
+            object.traverse( function ( child ) {
+                if ( child instanceof THREE.Mesh ) {
+                    console.log(child);
+                    // child.material.side = THREE.DoubleSide;    
+                    
+                    if (child.name === 'Mark') {
+                        child.material = redMaterial  
+                    }
+                    if (child.name === 'Earthdots') {
+                        child.material = whiteMaterial  
+                    }
+                }
+            });
+
             EarthObj.add(object)
         });
         scene.add(EarthObj)
-
-        canvas.addEventListener('mousemove', mouseMoveHandler)
-        canvas.addEventListener('mousedown', mouseDownHandler)
-        canvas.addEventListener('mouseup', mouseUpHandler)
-
-        canvas.addEventListener("touchstart", touchStartHandler);
-    	canvas.addEventListener("touchmove", touchMoveHandler);    
-    	canvas.addEventListener("touchend", mouseUpHandler);
-
-    	canvas.addEventListener("wheel", wheelEventHandler);
 
         window.addEventListener('resize', onCanvasResize)
 
         animate()
     }
-}
-
-function mouseMoveHandler(e) {    
-    if (mouseData.isClicked) {
-        const deltaX = Math.sign(e.x - mouseData.x) * settings.moveStep.x
-        const deltaY = Math.sign(e.y - mouseData.y) * settings.moveStep.y
-
-        mouseData.x = e.x
-        mouseData.y = e.y
-
-        // helicopterObj.rotation.z += deltaX
-        if (helicopterObj.rotation.z + deltaX < settings.maxXRotate && helicopterObj.rotation.z + deltaX > settings.minXRotate)
-            helicopterObj.rotation.z += deltaX
-        if (helicopterObj.rotation.x + deltaY < settings.maxYRotate && helicopterObj.rotation.x + deltaY > settings.minYRotate)
-            helicopterObj.rotation.x += deltaY
-    }
-}
-
-function mouseDownHandler(e) {
-    mouseData.isClicked = true
-    mouseData.x = e.x
-    mouseData.y = e.y
-}
-
-function mouseUpHandler(e) {
-    mouseData.isClicked = false
-    mouseData.isTouchScale = false
-}
-
-function touchStartHandler(e) {
-    e.preventDefault();
-    let evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
-	let touch = evt.touches[0] || evt.changedTouches[0];
-
-    mouseData.isClicked = true
-    mouseData.x = parseInt(touch.pageX)
-    mouseData.y = parseInt(touch.pageY) 
-
-    if (evt.touches.length > 1) {
-        mouseData.isTouchScale = true
-        let touch2 = evt.touches[1] || evt.changedTouches[1];
-
-        mouseData.x = Math.max(parseInt(touch.pageX), parseInt(touch2.pageX))
-    }
-}
-
-function touchMoveHandler(e) {
-    e.preventDefault();
-    
-    if (mouseData.isClicked) {
-        let evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
-        let touch = evt.touches[0] || evt.changedTouches[0];
-
-        if (!mouseData.isTouchScale) {
-            //rotate
-            const deltaX = Math.sign(parseInt(touch.pageX) - mouseData.x) * settings.moveStep.x
-            const deltaY = Math.sign(parseInt(touch.pageY) - mouseData.y) * settings.moveStep.y
-    
-            // helicopterObj.rotation.z += deltaX
-            if (helicopterObj.rotation.z + deltaX < settings.maxXRotate && helicopterObj.rotation.z + deltaX > settings.minXRotate)
-                helicopterObj.rotation.z += deltaX
-            if (helicopterObj.rotation.x + deltaY < settings.maxYRotate && helicopterObj.rotation.x + deltaY > settings.minYRotate)
-                helicopterObj.rotation.x += deltaY
-        } else {
-            if (evt.touches.length > 1) {
-                let touch2 = evt.touches[1] || evt.changedTouches[1];
-                let pageX = Math.max(parseInt(touch.pageX), parseInt(touch2.pageX))
-
-                // scale
-                const currentScale = Math.sign(mouseData.x - pageX) * settings.zoomStep
-                const newZoomVal = camera.position.z + currentScale
-    
-                if (newZoomVal < settings.zoomMax && newZoomVal > settings.zoomMin)
-                    camera.position.z += currentScale
-            }
-        }
-
-        mouseData.x = parseInt(touch.pageX)
-        mouseData.y = parseInt(touch.pageY)
-    }
-}
-
-function wheelEventHandler(e) {
-    e.preventDefault()
-    const newZoomVal = camera.position.z + Math.sign(e.deltaY) * settings.zoomStep
-
-    if (newZoomVal < settings.zoomMax && newZoomVal > settings.zoomMin)
-        camera.position.z += Math.sign(e.deltaY) * settings.zoomStep
 }
 
 function onCanvasResize() {
@@ -197,9 +111,27 @@ function onCanvasResize() {
     renderer.setSize( sceneSize.width, sceneSize.height );
 }
 
+window.addEventListener('mousemove', e => {
+    const newDeltaX = Math.sign(e.x - mouseData.x) * settings.moveStep.x
+    mouseData.x = e.x
+    deltaX = EarthObj.rotation.x + newDeltaX
+});
+
 function animate() {
-    requestAnimationFrame(animate);
+    const step = .01
+    const damping = .00001
+    //for scroll-x rotation
+    if (Math.abs(deltaX - currentDeltaX) > step) {
+        currentDeltaX = currentDeltaX + (deltaX - currentDeltaX) * step
+    } else {
+        currentDeltaX = currentDeltaX + (deltaX - currentDeltaX) * (deltaX - currentDeltaX) * (deltaX - currentDeltaX) * damping
+    }
+    EarthObj.rotation.y = currentDeltaX * Math.PI
+    
+    // camera.updateMatrixWorld();    
+    // camera.updateProjectionMatrix();
     renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
 
 export default App;
